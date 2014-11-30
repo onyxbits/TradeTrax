7f321f092d21d0daab40cf533b2c0e867521720d
patrick <github@onyxbits.de>
2014-11-30 19:58:44 +0100

    version bump

--- src/main/java/de/onyxbits/tradetrax/services/QaModule.java
+++ src/main/java/de/onyxbits/tradetrax/services/QaModule.java
@@ -38,6 +38,6 @@ public class QaModule
         // assets. Web browsers will cache assets because of the far future expires
         // header. If existing assets are changed, the version number should also
         // change, to force the browser to download new versions.
-        configuration.add(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT-QA");
+        configuration.add(SymbolConstants.APPLICATION_VERSION, "1.2-SNAPSHOT-QA");
     }
 }
