a05c524814b79b6ec84808833e025fbdcc0773ff
patrick <github@onyxbits.de>
2014-12-28 04:27:58 +0100

    New feature: search in logs

--- src/main/java/de/onyxbits/tradetrax/pages/tools/LedgerLog.java
+++ src/main/java/de/onyxbits/tradetrax/pages/tools/LedgerLog.java
@@ -3,14 +3,20 @@ package de.onyxbits.tradetrax.pages.tools;
 import java.text.DateFormat;
 import java.util.List;
 
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.beaneditor.BeanModel;
 import org.apache.tapestry5.beaneditor.PropertyModel;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.TextField;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.BeanModelSource;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 import de.onyxbits.tradetrax.entities.LogEntry;
 import de.onyxbits.tradetrax.services.SettingsStore;
@@ -25,14 +31,13 @@ public class LedgerLog {
 
 	@Property
 	private LogEntry logEntry;
-	
-	@Property 
+
+	@Property
 	private LogEntry row;
-	
 
 	@Inject
 	private Session session;
-	
+
 	@Inject
 	private BeanModelSource logSource;
 
@@ -41,30 +46,44 @@ public class LedgerLog {
 
 	@Inject
 	private Messages messages;
-	
+
+	@Property
+	@Persist
+	private String filter;
+
+	@Component(id = "filter")
+	private TextField filterField;
+
+	@Component(id = "filterForm")
+	private Form filterForm;
+
 	private DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
 	private DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.MEDIUM);
-	
+
 	public String getFullTime() {
-		return dateFormat.format(row.getTimestamp())+ " "+timeFormat.format(row.getTimestamp());
+		return dateFormat.format(row.getTimestamp()) + " " + timeFormat.format(row.getTimestamp());
 	}
-	
+
 	public BeanModel<LogEntry> getLogModel() {
 		BeanModel<LogEntry> model = logSource.createDisplayModel(LogEntry.class, messages);
 		List<String> lst = model.getPropertyNames();
 		for (String s : lst) {
 			try {
-			PropertyModel nameColumn = model.getById(s);
-			nameColumn.sortable(false);
+				PropertyModel nameColumn = model.getById(s);
+				nameColumn.sortable(false);
 			}
-			catch (Exception e){}
+			catch (Exception e) {
+			}
 		}
 		return model;
 	}
-	
-	
+
 	@SuppressWarnings("unchecked")
 	public List<LogEntry> getLogentries() {
-		return session.createCriteria(LogEntry.class).addOrder(Order.desc("timestamp")).list();
+		Criteria ret = session.createCriteria(LogEntry.class).addOrder(Order.desc("timestamp"));
+		if (filter!=null && filter.length()!=0) {
+			ret.add(Restrictions.ilike("details","%"+filter+"%"));
+		}
+		return ret.list();
 	}
 }
